ccaf4281cb5816d1f8a5fd78d5fa2b5c000b83cd
Miika Valkonen <miika.valkonen08@gmail.com>
2026-01-19 13:39:24 +0200

    database alustus

---
+++ mongo.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose")
+
+if (process.argv.length < 3) {
+    console.log("give password as argument")
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://miikavalkonen_db_user:${cYgLtMu09l7SXz3Q}@cluster0.iql4ixb.mongodb.net/?appName=Cluster0`
+
+mongoose.set("strictQuery", false)
+mongoose.connect(url, { family: 4 })
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'HTML is easy',
+  important: true,
+})
+
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
